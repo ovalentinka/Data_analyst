144fb34bdbe8325f3db3ac0cbf24916865fb3d8c
Karpova Oksana <112002911+ovalentinka@users.noreply.github.com>
2022-09-01 11:35:45 +0300

    Update 4_sql_tasks.sql

--- 4_sql_tasks.sql
+++ 4_sql_tasks.sql
@@ -207,3 +207,129 @@ SELECT p.user_id,
        (p.min_dt - u.creation_date) AS diff
 FROM stackoverflow.users AS u 
 JOIN p ON  u.id = p.user_id;
+
+'''
+14. Выведите общую сумму просмотров постов за каждый месяц 2008 года. 
+Если данных за какой-либо месяц в базе нет, такой месяц можно пропустить. Результат отсортируйте по убыванию общего количества просмотров.
+'''
+SELECT SUM(views_count),
+       DATE_TRUNC('month', creation_date)::date AS mnth
+FROM stackoverflow.posts
+GROUP BY DATE_TRUNC('month', creation_date)::date
+ORDER BY SUM(views_count) DESC;
+
+'''
+15. Выведите имена самых активных пользователей, которые в первый месяц после регистрации (включая день регистрации) дали больше 100 ответов. 
+Вопросы, которые задавали пользователи, не учитывайте. Для каждого имени пользователя выведите количество уникальных значений user_id. 
+Отсортируйте результат по полю с именами в лексикографическом порядке.
+'''
+SELECT u.display_name,
+       COUNT(DISTINCT p.user_id)
+FROM stackoverflow.posts AS p
+JOIN stackoverflow.users AS u ON p.user_id = u.id
+JOIN stackoverflow.post_types AS pt ON pt.id = p.post_type_id
+WHERE p.creation_date::date BETWEEN u.creation_date::date AND (u.creation_date::date + INTERVAL '1 month')
+   AND pt.type LIKE '%Answer%'
+GROUP BY u.display_name
+HAVING COUNT(p.id) > 100
+ORDER BY u.display_name;
+
+'''
+16. Выведите количество постов за 2008 год по месяцам. 
+Отберите посты от пользователей, которые зарегистрировались в сентябре 2008 года и сделали хотя бы один пост в декабре того же года. 
+Отсортируйте таблицу по значению месяца по убыванию.
+'''
+WITH t AS (
+           SELECT u.id
+           FROM stackoverflow.posts AS p
+           JOIN stackoverflow.users AS u ON p.user_id = u.id
+           WHERE DATE_TRUNC('month', u.creation_date)::date = '2008-09-01'
+              AND DATE_TRUNC('month', p.creation_date)::date = '2008-12-01'
+           GROUP BY u.id
+           HAVING COUNT(p.id)>0
+          )
+
+SELECT COUNT(p.id),
+       DATE_TRUNC('month', p.creation_date)::date      
+FROM stackoverflow.posts AS p
+WHERE p.user_id IN (SELECT * FROM t)
+   AND DATE_TRUNC('year', p.creation_date)::date = '2008-01-01'
+GROUP BY DATE_TRUNC('month', p.creation_date)::date
+ORDER BY DATE_TRUNC('month', p.creation_date)::date DESC;
+
+'''
+17. Используя данные о постах, выведите несколько полей:
+идентификатор пользователя, который написал пост;
+дата создания поста;
+количество просмотров у текущего поста;
+сумму просмотров постов автора с накоплением.
+Данные в таблице должны быть отсортированы по возрастанию идентификаторов пользователей, 
+а данные об одном и том же пользователе — по возрастанию даты создания поста.
+'''
+SELECT user_id,
+       creation_date,
+       views_count,
+       SUM(views_count) OVER (PARTITION BY user_id ORDER BY creation_date)						
+FROM stackoverflow.posts;
+
+'''
+18. Сколько в среднем дней в период с 1 по 7 декабря 2008 года включительно пользователи взаимодействовали с платформой? 
+Для каждого пользователя отберите дни, в которые он или она опубликовали хотя бы один пост. 
+Нужно получить одно целое число — не забудьте округлить результат.
+'''
+SELECT ROUND(AVG(t.cnt))
+FROM (
+      SELECT user_id,
+             COUNT(DISTINCT creation_date::date)  AS cnt
+      FROM stackoverflow.posts
+      WHERE creation_date::date BETWEEN '2008-12-01' AND '2008-12-07' 
+      GROUP BY user_id
+) AS t
+
+'''
+19. На сколько процентов менялось количество постов ежемесячно с 1 сентября по 31 декабря 2008 года? Отобразите таблицу со следующими полями:
+номер месяца;
+количество постов за месяц;
+процент, который показывает, насколько изменилось количество постов в текущем месяце по сравнению с предыдущим.
+Если постов стало меньше, значение процента должно быть отрицательным, если больше — положительным. Округлите значение процента до двух знаков после запятой.
+Напомним, что при делении одного целого числа на другое в PostgreSQL в результате получится целое число, 
+округлённое до ближайшего целого вниз. Чтобы этого избежать, переведите делимое в тип numeric.
+'''
+WITH t AS (
+SELECT EXTRACT(MONTH from creation_date::date) AS month,
+       COUNT(DISTINCT id)    
+FROM stackoverflow.posts
+WHERE creation_date::date BETWEEN '2008-09-01' AND '2008-12-31'
+GROUP BY month
+)
+
+SELECT *,
+       ROUND(((count::numeric / LAG(count) OVER (ORDER BY month)) - 1) * 100,2) AS user_growth
+FROM t;
+
+'''
+20. Выгрузите данные активности пользователя, который опубликовал больше всего постов за всё время. Выведите данные за октябрь 2008 года в таком виде:
+номер недели;
+дата и время последнего поста, опубликованного на этой неделе.
+'''
+WITH t AS (
+SELECT user_id,
+       COUNT(DISTINCT id) AS cnt
+FROM stackoverflow.posts
+GROUP BY user_id
+ORDER BY cnt DESC
+LIMIT 1),
+
+     t1 AS (
+SELECT p.user_id,
+       p.creation_date,
+       extract('week' from p.creation_date) AS week_number
+FROM stackoverflow.posts AS p
+JOIN t ON t.user_id = p.user_id
+WHERE DATE_TRUNC('month', p.creation_date)::date = '2008-10-01'
+           )
+
+SELECT DISTINCT week_number::numeric,
+       MAX(creation_date) OVER (PARTITION BY week_number)
+FROM t1
+ORDER BY week_number;
